ebb850ac068fe6661203120c18faa183e1dac641
dader34 <56900960+dader34@users.noreply.github.com>
2023-11-12 18:53:26 -0800

    Passed all tests

--- server/app.py
+++ server/app.py
@@ -21,7 +21,27 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def earthquake_by_id(id):
+    if id:
+        quake = Earthquake.query.filter_by(id=id).first()
+        if quake:
+            body = quake.to_dict()
+            status = 200
 
+        else:
+            body = {'message': f'Earthquake {id} not found.'}
+            status = 404
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<magnitude>')
+def mag(magnitude):
+    quakes = [quake.to_dict() for quake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()]
+    body = {'count': len(quakes),
+            'quakes': quakes
+            }
+    return make_response(body, 200)
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
